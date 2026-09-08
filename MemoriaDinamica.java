5edcdb7e3b3820dcb76e28e8b665826571563363
AbrahamMed =) <abraham.ernesto1234@gmail.com>
2026-09-07 18:06:40 -0600

    Add files via upload

---
+++ MemoriaDinamica.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class MemoriaDinamica {
+    public static void main(String[] args) {
+        ArrayList<String> frutas = new ArrayList<>();
+        frutas.add("Mango");
+        frutas.add("Manzana");
+        frutas.add("Banana");
+        frutas.add("Uvas");
+        System.out.println(frutas);
+
+        frutas.remove(0);
+        frutas.remove(1);
+        
+        frutas.add("Sandía");
+        System.out.println(frutas);
+    }
+}
